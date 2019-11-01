256b31d6fa55c0ab5c6cb47ca2bf285c45287be6
Emilio Wuerges <wuerges@gmail.com>
2019-11-01 11:32:16 -0300

    global object

--- include/global.hpp
+++ include/global.hpp
@@ -1,1 +1,34 @@
 #pragma once
+
+#include <ast.hpp>
+#include <base.hpp>
+
+#include <map>
+#include <vector>
+
+using std::map, std::vector, std::pair;
+
+namespace base{
+
+struct Track {
+    R2 segment; // the rectangle for this track;
+    vector<pair<int, Track*>> crossings; // the tracks that cross this track in the upper or lower layer
+};
+
+struct Layer {
+    bool horizontal;
+    vector<Track> tracks;
+};
+
+struct Router {
+    // parameters should be variables here
+    ast::Parameters parameters;
+    R2 boundary;
+
+    vector<Layer> layers;
+
+};
+
+Router router;
+
+}
